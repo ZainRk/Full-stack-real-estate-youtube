8dde345e93fedf6e9e44600f917396f2948e0934
ZainRk <ahmadrao925@gmail.com>
2023-07-20 19:56:08 +0500

    baseURL updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://full-stack-real-estate-youtube.vercel.app/api",
 });
 
 export const getAllProperties = async () => {
